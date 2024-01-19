f5f9e464a2e4e1334409768aa360ea2d33a897d4
MortonPL <bartm12@wp.pl>
2024-01-19 22:52:03 +0100

    Add AA verison packing

--- PackAll.bat
+++ PackAll.bat
@@ -1,9 +1,16 @@
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLRelease
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNARelease
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXRelease --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLRelease --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLRelease --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNARelease --version-suffix aa
 
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLDebug
-dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNADebug
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsDXDebug --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c UniversalGLDebug --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsGLDebug --version-suffix aa
+dotnet pack .\Rampastring.XNAUI.csproj -c WindowsXNADebug --version-suffix aa
+
+mkdir nupkg
+pushd bin
+FOR /r %%a in (*.*nupkg) do (
+  COPY "%%a" "..\nupkg\%%~nxa"
+)
+popd
